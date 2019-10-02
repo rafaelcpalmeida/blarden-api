e223a370e82fd2601ef5bba8601de895f07d9901
Rafael Almeida <rafaelcpalmeida@gmail.com>
2019-10-02 23:37:46 +0100

    Refactor database connection method

--- db/service.go
+++ db/service.go
@@ -8,13 +8,18 @@ import (
 )
 
 func DatabaseHandler() *gorm.DB {
-	db, err := gorm.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
-		os.Getenv("POSTGRESQL_HOST"),
-		os.Getenv("POSTGRESQL_PORT"),
-		os.Getenv("POSTGRESQL_USER"),
-		os.Getenv("POSTGRESQL_DBNAME"),
-		os.Getenv("POSTGRESQL_PASSWORD"),
-	))
+	databaseUrl := os.Getenv("DATABASE_URL")
+	if len(databaseUrl) == 0 {
+		databaseUrl = fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable",
+			os.Getenv("POSTGRESQL_HOST"),
+			os.Getenv("POSTGRESQL_PORT"),
+			os.Getenv("POSTGRESQL_USER"),
+			os.Getenv("POSTGRESQL_DBNAME"),
+			os.Getenv("POSTGRESQL_PASSWORD"),
+		)
+	}
+
+	db, err := gorm.Open("postgres", databaseUrl)
 
 	if err != nil {
 		fmt.Println(err)
