e43b09d23eb39ef7a019d689358e9f509eb4d0b8
Rafael Almeida <rafaelcpalmeida@gmail.com>
2019-10-18 00:04:09 +0100

    Add better logging

--- src/api/door/door.handler.go
+++ src/api/door/door.handler.go
@@ -3,6 +3,7 @@ package door
 import (
 	"blarden-api/src/api"
 	"blarden-api/src/services"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 	"net/http"
@@ -14,7 +15,7 @@ func Open(c *gin.Context) {
 	requestResponse, err := services.RequestOpenDoor()
 
 	if err != nil {
-		log.Error("Open door request errored")
+		log.Error(fmt.Sprintf("Open door request errored. Error: %s", err))
 		api.ErrorResponse(c, "error", "Door was unable to be opened",
 			http.StatusBadRequest)
 		return
